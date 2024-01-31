1959d2f0546771b0d72c24f1a3fa2b85dff9f6ff
Eduar Romero <el.g21@hotmail.com>
2024-01-31 15:17:45 -0500

    Creación de componente para sección de tecnologias

---
+++ src/pages/technologies/Technologies.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './stylesTechnologies.css'
+
+function Technologies() {
+  return (
+    <>
+    <div className="container bg-success mt-5">
+    <h1>Technologies Components</h1>
+    </div>
+    </>
+  )
+}
+
+export default Technologies
