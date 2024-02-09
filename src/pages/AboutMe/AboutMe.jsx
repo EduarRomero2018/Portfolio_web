f79c6d37dfc7aecf971fd45d7d84a7ef450317dd
Eduar Romero <el.g21@hotmail.com>
2024-02-08 21:06:54 -0500

    Se cambió la imagen del sobre mi

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./stylosAboutMe.css";
 
 // import valores from '../../Images/Nuestros Valores.webp';
-import user from "../../assets/images/user.png";
+import developer from "../../assets/images/webdeveloper.png";
 import { BsFillBookmarkCheckFill} from "react-icons/bs";
 
 
@@ -11,8 +11,17 @@ const Article1 = () => {
   return (
     <>
 
-      <div className="container ">
+      <div className="container">
         <div className="row text-center justify-content-center align-content-center">
+
+
+          <div className="col-sm-5">
+            <figure className=''>
+              <img src={developer} alt="developer" className="styles_img"/>
+            </figure>
+              <svgdeveloper></svgdeveloper>
+
+          </div>
 
           <div className="col-sm-7">
             <div className="card-body style_border1">
@@ -20,12 +29,6 @@ const Article1 = () => {
               <h5 className="card-title">Sobre mi</h5>
               <p className="card-text secondary_title">¡Hola! Soy un <span className='styles_span'>ingeniero de sistemas</span> con una formación en <span className='styles_span'>desarrollo web FullStack,</span> apasionado en todo lo que hago, dispuesto siempre a enfrentar desafíos, amante de todo lo que indica, <span className='styles_span'>transformación digital</span> y <span className='styles_span'>accesibilidad</span> para lograr una verdadera inclusión. ¿Mi punto único? La versatilidad: lo que me ha impulsado para adaptarme a los entornos de trabajo.</p>
             </div>
-          </div>
-
-          <div className="col-sm-5">
-            <figure>
-              <img src={user} alt="" className="styles_img "/>
-            </figure>
           </div>
 
         </div>
